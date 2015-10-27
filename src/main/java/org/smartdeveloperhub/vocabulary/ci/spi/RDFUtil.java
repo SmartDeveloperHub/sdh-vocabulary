e50bcd3781e4a6afda62bb102111c93ec799192b
Miguel Esteban Gutiérrez <m.esteban.gutierrez@gmail.com>
2015-10-27 09:25:44 +0100

    Bumped version number to 0.2.0-SNAPSHOT

--- src/main/java/org/smartdeveloperhub/vocabulary/ci/spi/RDFUtil.java
+++ src/main/java/org/smartdeveloperhub/vocabulary/ci/spi/RDFUtil.java
@@ -20,8 +20,8 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
- *   Artifact    : org.smartdeveloperhub.vocabulary:sdh-vocabulary:0.1.0
- *   Bundle      : sdh-vocabulary-0.1.0.jar
+ *   Artifact    : org.smartdeveloperhub.vocabulary:sdh-vocabulary:0.2.0-SNAPSHOT
+ *   Bundle      : sdh-vocabulary-0.2.0-SNAPSHOT.jar
  * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
  */
 package org.smartdeveloperhub.vocabulary.ci.spi;
