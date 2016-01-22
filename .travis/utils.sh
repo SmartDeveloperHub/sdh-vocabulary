74d6ff5f37e65f93d684c4df79a88a807283a17d
Miguel Esteban Gutiérrez <m.esteban.gutierrez@gmail.com>
2016-01-22 09:25:03 +0100

    Improved Travis CI Build configuration, continued
    - Updated ".travis/build.sh" script to enforce snapshot update when
      executing the Maven build.
    - Fixed path to encrypted keys used in the ".travis/utils.sh" script.

--- .travis/utils.sh
+++ .travis/utils.sh
@@ -30,9 +30,9 @@ function decryptKeys() {
   if [ "$1" != "porcelain" ];
   then
     echo "Decrypting private key..."
-    openssl aes-256-cbc -pass pass:"$2" -in config/src/main/resources/ci/secring.gpg.enc -out local.secring.gpg -d
+    openssl aes-256-cbc -pass pass:"$2" -in target/config/ci/secring.gpg.enc -out local.secring.gpg -d
     echo "Decrypting public key..."
-    openssl aes-256-cbc -pass pass:"$2" -in config/src/main/resources/ci/pubring.gpg.enc -out local.pubring.gpg -d
+    openssl aes-256-cbc -pass pass:"$2" -in target/config/ci/pubring.gpg.enc -out local.pubring.gpg -d
   else
     echo "Skipped key decription"
   fi
