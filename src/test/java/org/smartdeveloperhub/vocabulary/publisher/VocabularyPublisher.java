d2c16551a5d7e25d306d75ff45f7d43c1c79abb7
Miguel Esteban Gutiérrez <m.esteban.gutierrez@gmail.com>
2016-03-17 12:49:08 +0100

    Improved Publisher usability
    - Added two profiles for simplifying the execution the Publisher:
      1. 'publisher' which directly triggers the execution of the publisher
      using the Exec Maven Plugin. It requires specifying the application
      arguments using the 'exec.args' system property (See plugin details in
      http://www.mojohaus.org/exec-maven-plugin/java-mojo.html)
      2. 'client' which a part of the build produces a collection of command
      line clients leveraging the AppAssembler Maven Plugin. The produced
      artifacts are in the 'target/client' directory. In particular, two
      scripts are available in the 'bin' directory:
        - 'publisher.sh' to be executed in *nix base environments.
        - 'publisher.bat' to be executed in Windows based environments.
      Non of the profiles is active by default, thus, users interested in
      their execution must instruct Maven to do so.
    - The Publisher now provides a user-friendly description of the Catalog via
      standard output.
    - In case of failure the Publisher no generates a small log file with the
      details about the context in which it was executed. The name of the
      produced file fits the following regex: 'error.YYYYMMDD.hhmm.sss.log'.

--- src/test/java/org/smartdeveloperhub/vocabulary/publisher/VocabularyPublisher.java
+++ src/test/java/org/smartdeveloperhub/vocabulary/publisher/VocabularyPublisher.java
@@ -37,6 +37,7 @@ import io.undertow.util.Headers;
 import io.undertow.util.Methods;
 import io.undertow.util.StatusCodes;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -44,6 +45,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 import org.smartdeveloperhub.vocabulary.publisher.handlers.Attachments;
@@ -55,12 +57,17 @@ import org.smartdeveloperhub.vocabulary.util.Module;
 import org.smartdeveloperhub.vocabulary.util.Result;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 
 public class VocabularyPublisher {
 
-	public static void main(final String... args) {
+	public static void main(final String... args) throws FileNotFoundException, IOException {
 		if(args.length!=2) {
-			System.err.printf("Invalid argument number: required 2 arguments (%d)%n", args.length);
+			System.err.printf("Invalid argument number: 2 arguments required (%d)%n", args.length);
+			System.err.printf("USAGE: %s <path-to-ontologies> <base-namespace>%n",AppAssembler.applicationName(VocabularyPublisher.class));
+			System.err.printf(" <path-to-ontologies> : Path where the ontologies to publish are available.%n");
+			System.err.printf(" <base-namespace>     : Base namespace of the ontologies to publish.%n");
+			Application.logContext(args);
 			System.exit(-1);
 		}
 		try {
@@ -69,13 +76,12 @@ public class VocabularyPublisher {
 			final Result<Catalog> result = Catalogs.loadFrom(root, base);
 			if(result.isAvailable()) {
 				final Catalog catalog = result.get();
-				System.out.printf("Found %d modules%n",catalog.size());
-				for(final String moduleId:catalog.modules()) {
-					final Module module = catalog.resolve(URI.create(moduleId));
-					System.out.printf("- %s%n", module);
+				showCatalog(catalog);
+				try {
+					publish(catalog);
+				} finally {
+					System.out.println("Publisher terminated.");
 				}
-				publish(catalog);
-				System.out.println("Publisher terminated.");
 			} else {
 				System.err.println("Could not prepare catalog:\n"+result);
 				System.exit(-5);
@@ -91,6 +97,60 @@ public class VocabularyPublisher {
 			System.exit(-4);
 		}
 
+	}
+
+	private static void showCatalog(final Catalog catalog) {
+		System.out.printf("Found %d modules%n",catalog.size());
+		final List<Module> externals=Lists.newArrayList();
+		for(final String moduleId:catalog.modules()) {
+			final Module module = catalog.resolve(URI.create(moduleId));
+			if(module.isExternal()) {
+				externals.add(module);
+				continue;
+			}
+			showModule(module);
+		}
+		for(final Module module:externals) {
+			showDependency(module);
+		}
+	}
+
+	private static void showDependency(final Module module) {
+		final StringBuilder builder=
+			new StringBuilder("- [EXTERNAL] Module '").
+				append(module.location()).
+				append("' [").
+				append(module.relativePath()).
+				append("] (").
+				append(module.format().getName()).
+				append(")");
+		if(module.isOntology()) {
+			builder.append(" refers to ");
+			if(module.isVersion()) {
+				builder.append("version '").append(module.versionIRI()).append("' of ");
+			}
+			builder.append("IRI '").append(module.ontology()).append("'");
+			System.out.println(builder);
+		}
+	}
+
+	private static void showModule(final Module module) {
+		System.out.printf("- Module '%s':%n",module.location());
+		System.out.printf("  + Relative path: %s%n",module.relativePath());
+		System.out.printf("  + Format: %s%n",module.format().getName());
+		if(module.isOntology()) {
+			System.out.printf("  + Ontology:%n");
+			System.out.printf("    * IRI: %s%n",module.ontology());
+			if(module.isVersion()) {
+				System.out.printf("    * VersionIRI: %s%n",module.versionIRI());
+			}
+			if(module.hasImports()) {
+				System.out.printf("    * Imports:%n");
+				for(final String importedModule:module.imports()) {
+					System.out.printf("      - %s%n",importedModule);
+				}
+			}
+		}
 	}
 
 	private static void publish(final Catalog catalog) {
@@ -148,6 +208,7 @@ public class VocabularyPublisher {
 				}
 			}
 		}
+		System.out.println("<ENTER> detected.");
 	}
 
 }
